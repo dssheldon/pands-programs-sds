03fdd9f2f13754caf47f6f5876cc39c6c9ec35b6
dssheldon <sheldon1977@hotmail.com>
2020-02-21 20:08:36 +0000

    Added a while loop to check for a positive number input

--- squareroot.py
+++ squareroot.py
@@ -9,13 +9,20 @@ def sqrt():
 	
 	# asked for user input as per instructions. We can also ask for an argument 
 	# to be specified while defining the function
-	
-	positive_number = float(input('Please enter a positive number: '))
+	# added a while loop to validate whether to validate whether the number is positive.
+	# If the number is neagtive the program will display a message and repeat the loop asking for the number again.
+
+	is_positive = True
+	while is_positive:
+		positive_number = float(input('Please enter a positive number: '))
+		if positive_number < 1: # I chose 1 here as I also wanted to exclude 0 from the user input as it will give a 'div by zero' error
+			 print('The number you have entered is not a positive number')
+		else:
+			is_positive = False	 
+
 	
 	# The following code will be the first guess for the number. 
-	# This part was a bit tricky
-	# but after some research I was able to settle on half the intial number
-	# as a good first guess
+	# This part was a bit tricky but after some research I was able to settle on half the intial number as a good first guess
 	
 	positive_number_guess = (positive_number) / 2
 	
